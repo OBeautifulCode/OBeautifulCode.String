53caf37bb7698603fd37a93fb293fc582207d3f7
Suraj Gupta <surajg@gmail.com>
2016-08-03 20:29:21 -0400

    use appveyor built-in build features instead of custom build script

--- OBeautifulCode.String/Properties/AssemblyInfo.cs
+++ OBeautifulCode.String/Properties/AssemblyInfo.cs
@@ -12,11 +12,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OBeautifulCode.String")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OBeautifulCode.String")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("OBeautifulCode")]
 [assembly: AssemblyProduct("OBeautifulCode.String")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016 OBeautifulCode")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
